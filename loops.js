93c487cafe92d6f059874e15c9125a62942e2662
Jennifer E <je@jenniferevenson.com>
2018-01-11 07:34:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -30,14 +30,13 @@ function forLoop(jarray) {
 
 
 /* Reference parking lot 
-FOR syntax: 
-Use FOR when you know how many times you want the loop to run.
+FOR: Use when you know how many times you want the loop to run.
+
 for ([initialization]; [condition]; [iteration]) {
   [loopBody]
 }
 
-WHILE: Use when we don't know how many times the loop
-needs to reun because it depends on a function/return value.
+WHILE: Use when we don't know how many times the loop needs to reun because it depends on a function/return value.
 
 
 
