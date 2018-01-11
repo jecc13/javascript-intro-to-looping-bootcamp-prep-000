c045c49b9dd4c281354f27dea325aaf93aaee03d
Jennifer E <je@jenniferevenson.com>
2018-01-11 07:33:11 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,4 @@
-/*
-FOR syntax: 
-Use FOR when you know how many times you want the loop to run.
-for ([initialization]; [condition]; [iteration]) {
-  [loopBody]
-}
-*/
+
 jarray = [];
 
 function forLoop(jarray) {
@@ -31,4 +25,20 @@ function forLoop(jarray) {
   }
   return jarray;
 }
+*/
+
+
+
+/* Reference parking lot 
+FOR syntax: 
+Use FOR when you know how many times you want the loop to run.
+for ([initialization]; [condition]; [iteration]) {
+  [loopBody]
+}
+
+WHILE: Use when we don't know how many times the loop
+needs to reun because it depends on a function/return value.
+
+
+
 */
