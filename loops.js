1766553581dd3dc2f6a6723da84297ef82b47b4d
Jennifer E <je@jenniferevenson.com>
2018-01-11 07:28:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,27 +6,37 @@ for ([initialization]; [condition]; [iteration]) {
 }
 */
 jarray = [];
-kittens = [];
-
-function destructivelyAppendKitten(name) {
-  kittens.push(name);
-  return kittens;
-}
 
 function forLoop(jarray) {
     for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      // add "I am 1 strange loop." to the array.
-      // jarray.push('I am 1 strange loop.');
       jarray[i] = 'I am 1 strange loop.';
     } else {
       jarray[i] = 'I am ${i} strange loops.';
-      // jarray.push('I am ${i} strange loops.'); 
-      // add "I am ${i} strange loops."
+
     }
   }
   return jarray;
 }
+
+
+
+
+/* trying crap
+function forLoop(jarray) {
+    for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      jarray.push('I am 1 strange loop.');
+
+    } else {
+      jarray[i] = 'I am ${i} strange loops.';
+      jarray.push('I am ${i} strange loops.'); 
+
+    }
+  }
+  return jarray;
+}
+*/
 
 
 
