ef3bc8a11a018a81ee4ec7a1994ce0e075aefb10
Jennifer E <je@jenniferevenson.com>
2018-01-11 06:59:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,9 +10,9 @@ function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
       // add "I am 1 strange loop." to the array.
-      array.push = "I am 1 strange loop.";
+      array.push("I am 1 strange loop.");
     } else {
-      array.push = "I am ${i} strange loops."; 
+      array.push("I am ${i} strange loops."); 
       // add "I am ${i} strange loops."
     }
   }
